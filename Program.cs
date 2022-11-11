fe04230c2482b37e5d57e428d4f60e3be2895ef8
nata-ko <99805828+nata-ko@users.noreply.github.com>
2022-11-11 20:45:46 +0200

    Make required changes
    1) add enum for "Win" and "Loose" methods
    2) add class "Hostory" for saving game's history
    3) changed method "CheckRating" : now it returns only boolean values
    4) add method "GetException" that warning about exception(rating less than 0)
    5) changed way of saving points that were added/substracted from player's rating(history cunstructor)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿ //TR-11  Natalie Bratchykova
+  
  using System;
  class Program
 {
@@ -9,152 +10,110 @@
         GameAccount gameAccount2 = new GameAccount("Hayao");
         GameAccount gameAccount3 = new GameAccount("Mikael");
 
+
         gameAccount.playGame(gameAccount1);
+        gameAccount1.playGame(gameAccount);
+        gameAccount1.playGame(gameAccount2);
         gameAccount.playGame(gameAccount2);
         gameAccount.playGame(gameAccount3);
-        gameAccount.playGame(gameAccount2);
-        gameAccount.playGame(gameAccount1);
-        gameAccount.playGame(gameAccount1);
-        gameAccount.playGame(gameAccount2);
-        gameAccount.playGame(gameAccount2);
         gameAccount.playGame(gameAccount3);
+        gameAccount.playGame(gameAccount3);
+        gameAccount.playGame(gameAccount2);
 
         gameAccount1.playGame(gameAccount2);
         gameAccount1.playGame(gameAccount3);
         gameAccount1.playGame(gameAccount2);
         gameAccount1.playGame(gameAccount);
-        gameAccount1.playGame(gameAccount);
-        gameAccount1.playGame(gameAccount3);
-        gameAccount1.playGame(gameAccount3);
 
         gameAccount2.playGame(gameAccount);
         gameAccount2.playGame(gameAccount1);
         gameAccount2.playGame(gameAccount3);
-        gameAccount2.playGame(gameAccount1);
-        gameAccount2.playGame(gameAccount);
-        gameAccount2.playGame(gameAccount1);
-        gameAccount2.playGame(gameAccount3);
 
-
-        Console.WriteLine(gameAccount.getStatus());
-        Console.WriteLine(gameAccount1.getStatus());
-        Console.WriteLine(gameAccount2.getStatus());
-        Console.WriteLine(gameAccount3.getStatus());
+        Console.WriteLine(gameAccount.GetStatus());
+        Console.WriteLine(gameAccount1.GetStatus());
+        Console.WriteLine(gameAccount2.GetStatus());
+        Console.WriteLine(gameAccount3.GetStatus());
 
     }
 }
 
+enum GameResultStatus{win, loose};
 
 class GameAccount{
 
     public string UserName{get; set;}
     public int CurrentRating{get; set;}
- //save user's rating as a string
-    public string GameResultRating = "";
-    public int GamesCount;
- //save opponent's name
-    public string OpponentName = "";
-    private List<Game> gameAccountStatus = new List<Game>();
- //save result like "win" or "lose"
-    public string GameResult = "";
-
+    private List<History> gameAccountStatus = new List<History>();
 
     public GameAccount(string name){
         UserName = name;
-        GamesCount = 0;
-     //generate user's rating
-        CurrentRating = new Random().Next(1,100);
+        CurrentRating = new Random().Next(1,20);
     }
 
- //method for play game
     public void playGame(GameAccount opponent){
 
-     //create the Game's object
         Game game  = new Game(this, opponent);
-//create dices 
         int dice1 = new Random().Next(1,6);
-        int dice2 = new Random().Next(1,6);
+        
+        if (dice1 >= 3)
+        {
+            WinGame(opponent, game);
+        }
+        else
+        {
+            LooseGame(opponent, game);
+        }
+    }
 
+
+    public void WinGame(GameAccount opponent,  Game game){
+       // check if opponent's rating is more than 0 (if it's less output exeption)
+       GetException(CheckRating(opponent, game.GameRating), opponent);
+
+    // check his rating again and if it's less than 0 it's align new value for it = 1
+    // else it substracts game's rating
+        opponent.CurrentRating = (CheckRating(opponent, game.GameRating)) ? opponent.CurrentRating - game.GameRating : 1;
+    // add game rating to winner player's rating  
+        this.CurrentRating += game.GameRating;
+    // call PlayedGameResult method
+        PlayedGameResult(this, opponent, game);
+    }
+
+    public void LooseGame(GameAccount opponent,  Game game){
+        opponent.CurrentRating += game.GameRating;
+        GetException(CheckRating(this, game.GameRating), this);
+        this.CurrentRating = (CheckRating(this, game.GameRating)) ? this.CurrentRating - game.GameRating : 1;
+        PlayedGameResult(opponent, this,  game);
+
+    }
+
+// if after substraction player's rating become less than 0 returns false, else returns true
+    public Boolean CheckRating(GameAccount user, int gameRating){
+        return (user.CurrentRating - gameRating < 1) ? false: true;
+    }
+
+// if player's rating is less than 0 warning about error
+    public void GetException(Boolean checkRating, GameAccount player){
+        if(!checkRating){
+            Console.WriteLine($"ERROR! {player.UserName}'S RATING IS LESS THAN 0\nUPDATED {player.UserName}'S RATING = 1\n");
+        }
+    }
+
+
+    // add information about game results to list
+    public void PlayedGameResult(GameAccount user, GameAccount opponent, Game game){
      
-        if(dice1 > dice2){
-            WinGame(opponent, game.GameRating);
-        }
-        else if(dice1 < dice2){
-            LooseGame(opponent, game.GameRating);
-        }
-     //if dices have equal value we need to change it
-        else{
-            dice1 = new Random().Next(1,6);
-        }
+        user.gameAccountStatus.Add(new History(user, opponent, game, GameResultStatus.win));
+        opponent.gameAccountStatus.Add(new History(opponent, user, game, GameResultStatus.loose));
     }
 
-//method for victory
-    public void WinGame(GameAccount opponent, int GameRating){
-        this.OpponentName = opponent.UserName;//user's opponent -> opponent
-        opponent.OpponentName = this.UserName;// opponent's opponent -> user
-        
-        this.GameResult = "win"; //user -> win
-        opponent.GameResult = "loose"; // opponent -> loose
-
-        this.GameResultRating = "+" + GameRating.ToString();//user +
-        opponent.GameResultRating = "-" + GameRating.ToString();//opponent -
-
-//check opponent's rating
-        opponent.CurrentRating = (checkRating(opponent, GameRating)) ? 1: opponent.CurrentRating - GameRating;
-//add points to winner
-        this.CurrentRating += GameRating;
-//create Game's objects
-        Game winGame = new Game(this, opponent);
-        Game opWinGame = new Game(opponent, this);
-     //they need to have the same game's ID as it's the one game for both players
-        opWinGame.GameIdStr = winGame.GameIdStr;
-//add result of this method to both gamers' list
-        gameAccountStatus.Add(winGame); 
-        opponent.gameAccountStatus.Add(opWinGame);
-
-    }
-
-    public void LooseGame(GameAccount opponent, int GameRating){
-
-        this.OpponentName = opponent.UserName;
-        opponent.OpponentName = this.UserName;
-
-        opponent.CurrentRating += GameRating;
-
-        opponent.GameResult = "win";
-        this.GameResult = "loose";
-
-
-        this.GameResultRating = "-" + GameRating.ToString();
-        opponent.GameResultRating = "+" + GameRating.ToString();
-
-
-        this.CurrentRating = (checkRating(this, GameRating)) ? this.CurrentRating - GameRating : 1;
-
-        Game loose = new Game(this, opponent);
-        Game opLoose = new Game(opponent, this);
-        
-        opLoose.GameIdStr = loose.GameIdStr;
-        gameAccountStatus.Add(loose);
-        opponent.gameAccountStatus.Add(opLoose);
-    }
-
- //method for checking user's rating
-    public Boolean checkRating(GameAccount user, int GameRating){
-
-        Boolean isChecked = false;
-     //if rating is less than 0 this method throw error. Else -> substracte rating
-        return isChecked = (user.CurrentRating < 0) ? throw new ArgumentOutOfRangeException(nameof(user.CurrentRating), $"{user.UserName}'s rating is less than 0"): isChecked = true;
-    }
-
- //method for getting status
-    public string getStatus(){
+    public string GetStatus(){
         var result = new System.Text.StringBuilder();
         result.AppendLine($"for current user - {this.UserName}\n");
         
         result.AppendLine("Game's ID\t\tPoints\tStatus\tRating\t\tOpponent\tOpponent's points\tOpponent's Status");
         foreach(var item in gameAccountStatus){
-            result.AppendLine($"{item.GameIdStr}\t\t{item.GameResultRating}\t{item.GameResult}\t{item.mainUserRating.ToString()}\t\t{item.OpponentName}\t\t{item.OpponentGameResultRating}\t\t\t{item.OpponentGameResult}");
+            result.AppendLine($"{item.GameIdStr}\t\t{item.UserGameResultRating}\t{item.UserGameStatus}\t{item.MainUserRating}\t\t{item.OpponentUser.UserName}\t\t{item.OpponentGameResultRating}\t\t\t{item.OpponentGameStatus}");
         }
 
         return result.ToString();
@@ -163,41 +122,60 @@ class GameAccount{
 
 
 class Game{
-    public static int GameId = 20042904;
+    private static int GameId = 20042904;
     public string GameIdStr;
     public int GameRating{get; set;}
-    public int mainUserRating;
-    public string GameResultRating;
-    public string GameRatingStr{get; set;}
-    public string GameResult{set; get;}
-    public string OpponentName{get; set;}
-    public int opponentUserRating;
-    public string OpponentGameResult{set; get;}
-    public string OpponentGameResultRating;
-    GameAccount mainUser;
-    GameAccount OpponentUser;
-
 
     public Game(GameAccount user, GameAccount opponentUser){
         GameIdStr = GameId.ToString();
-        GameId++;
-
-        mainUser = user;
-        this.OpponentUser = opponentUser;
-
-     //assign appropriate properties
-        mainUserRating = mainUser.CurrentRating;
-        opponentUserRating = OpponentUser.CurrentRating;
-
-        OpponentName = this.OpponentUser.UserName;
-        GameResult = mainUser.GameResult;
-        GameRatingStr = GameRating.ToString();
-
-        GameRating = new Random().Next(1, 15);
-        OpponentGameResult = opponentUser.GameResult;
-
-        GameResultRating = mainUser.GameResultRating;
-        OpponentGameResultRating = this.OpponentUser.GameResultRating;
+        GameId++;        
+        GameRating = new Random().Next(1, 15);    
     }
 
+}
+
+class History{
+    
+    // for save Game's ID  for outputting in game's history
+    public string GameIdStr;
+
+    public GameAccount MainUser;
+    public GameAccount OpponentUser;
+
+    public string UserGameResultRating;
+
+    public string UserGameStatus{set; get;}
+    public string OpponentGameStatus{set; get;}
+    
+    public int MainUserRating;
+       
+    public string OpponentGameResultRating;
+    
+
+        public History(GameAccount user, GameAccount opponentUser, Game game, GameResultStatus status){
+    
+        this.GameIdStr = game.GameIdStr;
+        MainUser = user;
+        this.OpponentUser = opponentUser;
+
+        MainUserRating = MainUser.CurrentRating;
+        
+       UserGameStatus = status.ToString();
+       
+       // check if user game's status == win
+       if(UserGameStatus.Equals("win")){
+        // if user's status = win, opponent's should be loose
+        OpponentGameStatus = GameResultStatus.loose.ToString();
+        // user won -> he got poitive result (add game's rating to his own)
+        UserGameResultRating = game.GameRating.ToString();
+        // opponent lost -> he got negative result (substract game's rating from his own)
+        OpponentGameResultRating = (game.GameRating * -1).ToString();
+       }
+
+       else{
+        OpponentGameStatus = GameResultStatus.win.ToString();
+        OpponentGameResultRating = game.GameRating.ToString();
+        UserGameResultRating = (game.GameRating * -1).ToString();
+       }
+        }
 }
